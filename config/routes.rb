5b0bc4ef9ea90478967be7e3e01e9f7a6e0ca3c1
Olivia Rice <orice@wisc.edu>
2014-01-30 18:26:17 -0600

    Finish demo app

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ DemoApp::Application.routes.draw do
 
   resources :users
 
-  resources :articles
-
-  root to: "articles#index"
+  root to: "users#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
